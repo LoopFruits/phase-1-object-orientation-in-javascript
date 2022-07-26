c1faad624fa354f429d95b9c6f3792f318e94843
LoopFruits <Kensworldc@gmail.com>
2022-07-26 16:12:35 -0400

    oo

---
+++ index.js
@@ -0,0 +1,11 @@
+class Lotr{
+    constructor(name,race,height){
+        this.name = name 
+        this.race = race
+        this.height = height
+        console.log(this)
+    }
+}
+
+// The instance of the object 
+new Lotr('aaragon','man','tall')
